6f6313504ee8ada49fe3604a41529a416720c5a1
MarioTataje <57409736+MarioTataje@users.noreply.github.com>
2020-07-03 02:41:34 -0500

    Revert "add iamge"
    This reverts commit 41a92b1ea5c85d4a8ec1fa76470354493bb6cf4a, reversing
    changes made to e61c36798fe0b30865521b1fb6637cb3a982bd83.

--- TF/Template.py
+++ TF/Template.py
@@ -5,13 +5,14 @@ from pygame.locals import (
 )
 
 
-class Instructions:
+class template:
+
     def __init__(self, width, height):
         pygame.init()
         self.screen_width = width
         self.screen_height = height
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
-        self.tittle = pygame.display.set_caption("Instructions")
+        self.tittle = pygame.display.set_caption("Standby")
         self.white = (255, 255, 255)
 
     def run(self, _running):
