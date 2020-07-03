d4216d79f99904001adc5e7b65eb96a25ac8e631
MarioTataje <57409736+MarioTataje@users.noreply.github.com>
2020-07-03 02:23:30 -0500

    standby

--- TF/instructions.py
+++ TF/instructions.py
@@ -3,6 +3,12 @@ from pygame.locals import (
     KEYDOWN,
     K_ESCAPE
 )
+from standby import Standby
+
+
+def menuGame():
+    menuView = Standby(800, 600)
+    menuView.run(True)
 
 
 class Instructions:
@@ -12,7 +18,35 @@ class Instructions:
         self.screen_height = height
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         self.tittle = pygame.display.set_caption("Instructions")
+        self.gray = (128, 128, 128)
+        self.newGray = (55, 118, 118)
         self.white = (255, 255, 255)
+        self.black = (0, 0, 0)
+
+    def text_objects(self, text, font):
+        textSurface = font.render(text, True, self.white)
+        return textSurface, textSurface.get_rect()
+
+    def button(self, x, y, width, height, text, action=None):
+        mouse2 = pygame.mouse.get_pos()
+        click2 = pygame.mouse.get_pressed()
+
+        if x + width > mouse2[0] > x and y + height > mouse2[1] > y:
+            pygame.draw.rect(self.screen, self.black, ((x - 10) - 2, (y - 10) - 2, (width + 20) + 4, (height + 20) + 4),
+                             0)
+            pygame.draw.rect(self.screen, self.newGray, (x - 10, y - 10, width + 20, height + 20))
+            word = pygame.font.SysFont('comicsans', 30)
+
+            if click2[0] == 1 and action is not None:
+                action()
+        else:
+            pygame.draw.rect(self.screen, self.black, (x - 2, y - 2, width + 4, height + 4), 0)
+            pygame.draw.rect(self.screen, self.gray, (x, y, width, height))
+            word = pygame.font.SysFont('comicsans', 25)
+
+        textSurf, textRect = self.text_objects(text, word)
+        textRect.center = ((x + int((width / 2))), (y + int((height / 2))))
+        self.screen.blit(textSurf, textRect)
 
     def run(self, _running):
         while _running:
@@ -22,6 +56,7 @@ class Instructions:
                     if event.key == K_ESCAPE:
                         _running = False
 
+            self.button(330, 380, 130, 40, 'Go', menuGame)
             pygame.display.update()
 
         pygame.quit()
